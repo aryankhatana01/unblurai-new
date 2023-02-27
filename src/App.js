07f426fb7fc7fdfd69f566e90d31538de3396134
aryankhatana01 <aryankhatana01@gmail.com>
2023-02-27 22:39:18 +0530

    created the features section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
+import Features from './components/features/Features';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Features />
     </div>
   );
 }
