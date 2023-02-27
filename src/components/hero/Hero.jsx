47d8862e2ee68e50b5173d75225912cc37171d4d
aryankhatana01 <aryankhatana01@gmail.com>
2023-02-27 21:55:46 +0530

    hero section done

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <>
+        <h1 className='main-heading'>
+            Upscale your <span className='images-text'>images</span> by 4x in <span className='just-one-click'>just one click</span>
+        </h1>
+        <p className='subheading'>Increase the size of your image using AI for completely free.</p>
+        <div className="btn-container">
+            <a className='try-btn' href="/home">
+                Try Now!
+            </a>
+        </div>
+    </>
+  )
+}
+
+export default Hero
