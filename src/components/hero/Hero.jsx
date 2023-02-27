8932ee7fb5c684c61519370f8c0d12d8138e2aa5
aryankhatana01 <aryankhatana01@gmail.com>
2023-02-27 22:12:26 +0530

    completed hero section with the section division line

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,7 +3,7 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <>
+    <div className='hero-section'>
         <h1 className='main-heading'>
             Upscale your <span className='images-text'>images</span> by 4x in <span className='just-one-click'>just one click</span>
         </h1>
@@ -13,7 +13,8 @@ const Hero = () => {
                 Try Now!
             </a>
         </div>
-    </>
+        <div className="section-division-line"></div>
+    </div>
   )
 }
 
