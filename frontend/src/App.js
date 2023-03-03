ae44910acafdcb3719053b31a115c8672a8f45f4
aryankhatana01 <aryankhatana01@gmail.com>
2023-03-03 09:20:00 +0530

    routing completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import Hero from './components/hero/Hero';
-import Features from './components/features/Features';
-import Tools from './components/tools/Tools';
+import HomePage from './pages/homepage/HomePage';
+import { Routes, Route } from 'react-router-dom';
+import Upscaler from './pages/upscaler/Upscaler';
+import Relight from './pages/relight/Relight';
+import RemoveBG from './pages/removebg/RemoveBG';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Hero />
-      <Features />
-      <Tools />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/upscaler" element={<Upscaler />} />
+        <Route path="/relight" element={<Relight />} />
+        <Route path="/removebg" element={<RemoveBG />} />
+      </Routes>
     </div>
   );
 }
