2a871a1103e6a1537af1d866ad0394bf3f2b7868
aryankhatana01 <aryankhatana01@gmail.com>
2023-03-03 09:19:46 +0530

    created a separate homepage component instead of adding it directly to App.js

---
+++ frontend/src/pages/homepage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../../components/hero/Hero';
+import Features from '../../components/features/Features';
+import Tools from '../../components/tools/Tools';
+
+const HomePage = () => {
+  return (
+    <>
+        <Hero />
+        <Features />
+        <Tools />
+    </>
+  )
+}
+
+export default HomePage
