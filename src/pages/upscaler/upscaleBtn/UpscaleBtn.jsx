cc9f1a4c4b8762bdf41136aea5407ddb276b2a39
aryankhatana01 <aryankhatana01@gmail.com>
2023-03-05 22:58:42 +0530

    tested the api upload integration with the frontend

--- src/pages/upscaler/upscaleBtn/UpscaleBtn.jsx
+++ src/pages/upscaler/upscaleBtn/UpscaleBtn.jsx
@@ -1,11 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import './UpscaleBtn.css';
 import FileContext from '../contexts/FileContext'
 
 
 const UpscaleBtn = () => {
     const selectedFile = useContext(FileContext);
-    // const [pred, setPred] = useState(null);
+    const [pred, setPred] = useState(null);
 
     const handleUpload = async () => {
         const formData = new FormData();
@@ -22,14 +22,14 @@ const UpscaleBtn = () => {
     }
     const handlePredict = async () => {
         await handleUpload();
-        // const requestOptions = {
-        //     method: 'GET',
-        // };
-        // const resp = await fetch(`http://localhost:8000/predict/?filename=${selectedFile.name}`, requestOptions);
-        // const data = await resp.json();
-        // setPred(data["Prediction"]);
-        // console.log(data);
-        // console.log(pred);
+        const requestOptions = {
+            method: 'GET',
+        };
+        const resp = await fetch(`http://localhost:8000/predict/?filename=${selectedFile.name}`, requestOptions);
+        const data = await resp.json();
+        setPred(data["STATUS"]);
+        console.log(data);
+        console.log(pred);
     }
     return (
         <div className="predict-button">
